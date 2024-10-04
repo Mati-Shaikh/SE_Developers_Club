909605aff4e3c8f2ab121c9a279a49e97146411d
Mati-Shaikh <matishaikh7@gmail.com>
2024-10-04 14:45:20 -0700

    Initial commit from Create Next App

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return (
+      <div className="bg-blue-600 text-white py-20">
+        <div className="container mx-auto px-4">
+          <h1 className="text-4xl font-bold mb-4">Welcome to Software Engineers Developer Club</h1>
+          <p className="text-xl">Join us in building the future of technology!</p>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Hero
