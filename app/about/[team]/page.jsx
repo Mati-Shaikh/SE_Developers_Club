0652dd7a0d234607ffb91748424c892bc861c0e7
MasoodGhauri <mmasoodghauri@gmail.com>
2024-10-16 10:44:48 +0500

    about team page

---
+++ app/about/[team]/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutTeamPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">About Our Club</h1>
+      <p>This is the about page for the TEAM.</p>
+    </div>
+  );
+}
