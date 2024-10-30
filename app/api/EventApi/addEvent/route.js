19a123f67a3443ddc3b48596bc5ea5aaac9cd2ca
MasoodGhauri <mmasoodghauri@gmail.com>
2024-10-30 11:39:14 +0500

    added default lock false

--- app/api/EventApi/addEvent/route.js
+++ app/api/EventApi/addEvent/route.js
@@ -6,21 +6,28 @@ export async function POST(req) {
   await dbConnect();
 
   try {
-    const { name, time, venue, capacity, lock, description, images } = await req.json();
+    const { name, time, venue, capacity, description, images } =
+      await req.json();
 
     const newEvent = new Event({
       name,
       time,
       venue,
       capacity,
-      lock,
+      lock: false,
       description,
       images,
     });
 
     await newEvent.save();
-    return NextResponse.json({ message: "Event created successfully", data: newEvent }, { status: 201 });
+    return NextResponse.json(
+      { message: "Event created successfully", data: newEvent },
+      { status: 201 }
+    );
   } catch (error) {
-    return NextResponse.json({ message: "Failed to create event", error: error.message }, { status: 500 });
+    return NextResponse.json(
+      { message: "Failed to create event", error: error.message },
+      { status: 500 }
+    );
   }
 }
